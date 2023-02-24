827eaaf362d3d22f963cc6fee166d86d710a8e24
Daniel29199 <108014367+Daniel29199@users.noreply.github.com>
2023-02-23 22:03:01 -0600

    cambios

--- Angular/Clase 6/miprimeraapp/src/app/empleado/empleado.component.ts
+++ Angular/Clase 6/miprimeraapp/src/app/empleado/empleado.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  // template:"<p>empleado works!</p>",
   styleUrls: ['./empleado.component.css']
+  // styles:["p{background-color:red;}"]
+
+
 })
-export class EmpleadoComponent {
+export class EmpleadoComponent implements OnInit {
+  constructor(){ }
+  ngOnInit(): void {
+      
+  }
 
 }
