2becd1bf6dd7f0caf88e97f49df0c02664926db6
Daniel29199 <108014367+Daniel29199@users.noreply.github.com>
2023-02-23 22:40:51 -0600

    cambios

---
+++ Angular/Clase 7/Interpolacion/miprimeraapp/src/app/empleado/empleado.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  // template:"<p>empleado works!</p>",
+  styleUrls: ['./empleado.component.css']
+  // styles:["p{background-color:red;}"]
+
+
+})
+export class EmpleadoComponent implements OnInit {
+
+  nombre="Juan";
+  Apellido="Diaz"
+  edad=8;
+  empresa="ABC";
+  constructor(){ }
+  ngOnInit(): void {
+      
+  }
+
+}
